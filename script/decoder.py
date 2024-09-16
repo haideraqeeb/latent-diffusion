e2aba86ac358bcda8decf1e197637fa4579b85b7
haideraqeeb <haiderraqeeb10@gmail.com>
2024-09-16 11:07:56 +0530

    create forward for decoder.py

--- script/decoder.py
+++ script/decoder.py
@@ -4,7 +4,32 @@ import torch.nn.functional as F
 from attention import SelfAttention
 
 class VAE_AttentionBlock(nn.Module):
-    def __init__(self, channels):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.groupnorm = nn.GroupNorm(32, channels)
-        self.attention = SelfAttention(1, channels)
+        self.groupnorm_1 = nn.GroupNorm(32, in_channels)
+        self.conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+
+        self.groupnorm_2 = nn.GroupNorm(32, in_channels)
+        self.conv_2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+        if in_channels == out_channels:
+            self.residual = nn.Identity()
+        else:
+            self.residual = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
+
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        residue = x
+
+        x = self.groupnorm_1(x)
+
+        x = F.silu(x)
+
+        x = self.conv_1(x)
+
+        x = self.groupnorm_2(x)
+
+        x = F.silu(x)
+
+        x = self.conv_2(x)
+
+        return x + self.residual(residue)
