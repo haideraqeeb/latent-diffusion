fe0c71a5ba6a6b810869469fdedc18cae594ee2c
haideraqeeb <haiderraqeeb10@gmail.com>
2024-09-25 00:01:07 +0530

    create model_converter.py

--- script/model_loader.py
+++ script/model_loader.py
@@ -8,3 +8,8 @@ def preload_models_from_standard_weights(ckpt_path, device):
 
      encoder = VAE_Encoder().to(device)
      encoder.load_state_dict(state_dict["encoder"], strict=True)
+
+     decoder = VAE_Decoder().to(device)
+     decoder.load_state_dict(state_dict["decoder"], strict=False)
+
+     
