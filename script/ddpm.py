2133a99970742ce9177d6216ff37dfb6b6ab2517
haideraqeeb <haiderraqeeb10@gmail.com>
2024-09-24 20:56:44 +0530

    add utility functions to ddpm sampler

--- script/ddpm.py
+++ script/ddpm.py
@@ -36,3 +36,52 @@ class DDPMSampler:
                               device = original_samples.device, dtype=original_samples.dtype)
           noisy_samples = (sqrt_alpha_prod * original_samples) + (sqrt_one_minus_alpha_prod) * noise
           return noisy_samples
+     
+     def _get_previous_timestep(self, timestep: int) -> int:
+          prev_t = timestep - (self.num_training_steps // self.num_inference_steps)
+          return prev_t
+
+     def step(self, timestep: int, latents: torch.Tensor, model_output: torch.Tensor):
+          t = timestep
+          prev_t = self._get_previous_timestep(t)
+
+          alpha_prod_t = self.alpha_cumprod[timestep]
+          alpha_prod_t_prev = self.alpha_cumprod[prev_t] if prev_t >=0 else self.one
+          beta_prod_t = 1 - alpha_prod_t
+          beta_prod_t_prev = 1 - alpha_prod_t_prev
+          current_alpha_t = alpha_prod_t / alpha_prod_t_prev
+          current_beta_t = 1 - current_alpha_t
+
+          pred_original_sample = (latents - beta_prod_t ** 0.5 * model_output) / alpha_prod_t ** 0.5
+
+          pred_original_sample_coeff = (alpha_prod_t_prev ** 0.5 * current_beta_t) / beta_prod_t
+          current_sample_coeff = current_alpha_t ** 0.5 * beta_prod_t_prev / beta_prod_t
+
+          pred_prev_sample = pred_original_sample_coeff * pred_original_sample + current_sample_coeff * latents
+
+          variance = 0
+          if t > 0:
+               device = model_output.device
+               noise = torch.randn(model_output.shape, generator=self.generator, device=device, dtype=model_output.dtype)
+
+               variance = (self._get_variance(t) ** 0.5) * noise
+          
+          pred_prev_sample = pred_prev_sample + variance
+          return pred_prev_sample
+
+     def _get_variance(self, timestep: int) -> torch.Tensor:
+          prev_t = self._get_previous_timestep(timestep)
+
+          alpha_prod_t = self.alpha_cumprod[timestep]
+          alpha_prod_t_prev = self.alpha_cumprod[prev_t] if prev_t >= 0 else self.one
+
+          current_beta_t = 1 - alpha_prod_t / alpha_prod_t_prev
+
+          variance = torch.clamp(variance, min=1e-20)
+
+          return variance
+     
+     def set_strength(self, strength=1):
+          start_step = self.num_inference_steps - int(self.num_inference_steps * strength)
+          self.timesteps = self.timesteps[start_step:]
+          self.start_step = start_step
