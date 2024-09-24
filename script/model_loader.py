1e4ecea8fbe946df49b1477b8f98c6d451bf05b3
haideraqeeb <haiderraqeeb10@gmail.com>
2024-09-25 00:07:27 +0530

    map model names for loading

--- script/model_loader.py
+++ script/model_loader.py
@@ -2,6 +2,7 @@ from clip import CLIP
 from encoder import VAE_Encoder
 from decoder import VAE_Decoder
 from diffusion import Diffusion
+import model_converter
 
 def preload_models_from_standard_weights(ckpt_path, device):
      state_dict = model_converter.load_from_standard_weights(ckpt_path, device)
@@ -12,4 +13,15 @@ def preload_models_from_standard_weights(ckpt_path, device):
      decoder = VAE_Decoder().to(device)
      decoder.load_state_dict(state_dict["decoder"], strict=False)
 
-     
+     diffusion = Diffusion().to(device)
+     diffusion.load_state_dict(state_dict["diffusion"], strict=True)
+
+     clip = CLIP().to(device)
+     clip.load_state_dict(state_dict["clip"], strict=True)
+
+     return{
+          "clip": clip,
+          "encoder": encoder,
+          "decoder": decoder,
+          "diffusion": diffusion
+     }
