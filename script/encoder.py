888c30528be7f07d93c99f20ffc37a229670254b
haideraqeeb <haiderraqeeb10@gmail.com>
2024-09-25 00:23:58 +0530

    fix import issue

--- script/encoder.py
+++ script/encoder.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
-from script.decoder import VAE_AttentionBlock, VAE_ResidualBlock
+from decoder import VAE_AttentionBlock, VAE_ResidualBlock
 
 class VAE_Encoder(nn.Sequential):
     def __init__():
