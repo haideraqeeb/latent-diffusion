422c7da7d5b0c966dcb349ed6c71de0bfd1ca8ca
haideraqeeb <haiderraqeeb10@gmail.com>
2024-09-26 23:41:48 +0530

    fix: typos and layer mappings to checkpoint file

--- script/clip.py
+++ script/clip.py
@@ -5,11 +5,11 @@ from attention import SelfAttention
 
 class CLIP(nn.Module):
      def __init__(self):
-
+          super().__init__()
           self.embedding = CLIPEmbedding(49408, 768, 77)
           
-          self.layers = nn.Module([
-               CLIPLayer(12, 768) in range(12)
+          self.layers = nn.ModuleList([
+               CLIPLayer(12, 768) for i in range(12)
           ])
 
           self.layernorm = nn.LayerNorm(768)
@@ -30,12 +30,12 @@ class CLIPEmbedding(nn.Module):
      def __init__(self, n_vocab, n_embd, n_tokens):
           super().__init__()
           self.token_embedding = nn.Embedding(n_vocab, n_embd)
-          self.postion_embedding = nn.Parameter(torch.zeros(n_tokens, n_embd))
+          self.position_embedding = nn.Parameter(torch.zeros(n_tokens, n_embd))
 
      def forward(self, tokens):
           
           x = self.token_embedding(tokens)
-          x += self.postion_embedding
+          x += self.position_embedding
 
           return x
      
@@ -43,25 +43,25 @@ class CLIPLayer(nn.Module):
      def __init__(self, n_heads, n_embd):
           super().__init__()
 
-          self.layernorm1 = nn.LayerNorm(n_embd)
+          self.layernorm_1 = nn.LayerNorm(n_embd)
           self.attention = SelfAttention(n_heads, n_embd)
-          self.layernorm2 = nn.LayerNorm(n_embd)
-          self.linear1 = nn.Linear(n_embd, 4*n_embd)
-          self.linear2 = nn.Linear(n_embd*4, n_embd)
+          self.layernorm_2 = nn.LayerNorm(n_embd)
+          self.linear_1 = nn.Linear(n_embd, 4*n_embd)
+          self.linear_2 = nn.Linear(n_embd*4, n_embd)
 
      def forward(self, x: torch.Tensor) -> torch.Tensor:
 
           residue = x
 
-          x = self.layernorm1(x)
-          x = self.attention(x)
+          x = self.layernorm_1(x)
+          x = self.attention(x, causal_mask=True)
           x += residue
 
           residue = x
-          x = self.layernorm2(x)
-          x = self.linear1(x)
+          x = self.layernorm_2(x)
+          x = self.linear_1(x)
           x = x * torch.sigmoid(1.702 * x)
-          x = self.linear2(x)
+          x = self.linear_2(x)
 
           x += residue
 
